53332100627dec3ea85741852e7a3c383de77186
Vladyslav Yeromenko <vy.skyp1nus@gmail.com>
2023-01-28 19:01:32 +0200

    10 - Scroll should not work during preloading

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,11 +1,19 @@
+function notScroll() {
+    let body = document.querySelector('html');
+    body.style.overflow = 'hidden';
+}
+
 function hiddenPreloading() {
+    let body = document.querySelector('html');
     let preloader = document.getElementById('preloader');
     preloader.classList.add('hide-preloader');
     setInterval(function() {
           preloader.classList.add('preloader-hidden');
     }, 1990);
+
+    body.style.overflow = "scroll";
 }
 
 waitingTime = setTimeout(hiddenPreloading, 3500);
 
-window.onload = () => { waitingTime; }
+window.onload = () => { notScroll(); waitingTime; }
