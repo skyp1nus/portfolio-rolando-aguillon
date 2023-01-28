a238fca2e2172b57edfdfc51c8b402718498c7ea
Vladyslav Yeromenko <vy.skyp1nus@gmail.com>
2023-01-28 14:30:30 +0200

    5 - Preloading

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,7 +1,11 @@
-window.onload = function() {
+function hiddenPreloading() {
     let preloader = document.getElementById('preloader');
     preloader.classList.add('hide-preloader');
     setInterval(function() {
           preloader.classList.add('preloader-hidden');
     }, 1990);
 }
+
+waitingTime = setTimeout(hiddenPreloading, 3500);
+
+window.onload = () => { waitingTime; }
