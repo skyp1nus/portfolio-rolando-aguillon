208e05f6ce4171ae4112d1e8d769c9c63e3081a0
Vladyslav Yeromenko <vy.skyp1nus@gmail.com>
2023-01-28 19:04:21 +0200

    v1.0.3 new path

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,19 @@
-window.onload = function() {
+function notScroll() {
+    let body = document.querySelector('html');
+    body.style.overflow = 'hidden';
+}
+
+function hiddenPreloading() {
+    let body = document.querySelector('html');
     let preloader = document.getElementById('preloader');
     preloader.classList.add('hide-preloader');
     setInterval(function() {
           preloader.classList.add('preloader-hidden');
     }, 1990);
+
+    body.style.overflow = "scroll";
 }
+
+waitingTime = setTimeout(hiddenPreloading, 3500);
+
+window.onload = () => { notScroll(); waitingTime; }
